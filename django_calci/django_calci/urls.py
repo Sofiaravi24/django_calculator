849085ee7541fbbd01f24885589d2c3826780e93
Sofiravi24 <sofiaravi24@gmail.com>
2023-08-27 16:43:09 +0530

    final_commit

--- django_calci/django_calci/urls.py
+++ django_calci/django_calci/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('calculator/', include('calci.urls')),
     path('', include('accounts.urls')),
 ]
 
